c8805ef8920e13df641a0cf301621d380a36ecf3
Shobana-123 <tshobi95@gmail.com>
2020-05-06 15:21:41 +0530

    Shobana-gmail: Test registered user Email Notification

--- development/views/pages/phpFolder/Php1.php
+++ development/views/pages/phpFolder/Php1.php
@@ -8,3 +8,4 @@ Testing developer email notification: 5th attempt
 Testing developer email notification: 6th attempt
 Testing developer email notification: 7th attempt
 Testing developer email notification: 8th attempt
+Testing developer email notification: 9th attempt

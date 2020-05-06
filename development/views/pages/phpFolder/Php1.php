f4ccee1427a03064992a243e3caffe0bb78b8389
s.c.thiruvengadam <s.c.thiruvengadam@accenture.com>
2020-05-06 14:29:52 +0530

    Shobana: Test Email Notification

--- development/views/pages/phpFolder/Php1.php
+++ development/views/pages/phpFolder/Php1.php
@@ -2,3 +2,4 @@ This is sample php file
 Testing email notification: May 6th
 Testing developer email notification: May 6th
 Testing developer email notification: 2nd attempt
+Testing developer email notification: 3rd attempt
